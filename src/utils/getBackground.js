80eda072affb837c083a33a0addd83d3598239ca
Tonatiuj Sánchez <tonsanzjimz@gmail.com>
2024-04-08 15:11:50 -0600

    Backgrounds agregados localmente

--- src/utils/getBackground.js
+++ src/utils/getBackground.js
@@ -4,53 +4,53 @@ export const getBackground = (weatherId) => {
 
     if (weatherId >= 200 && weatherId <= 250) {
         // thunder
-        return 'https://images.unsplash.com/photo-1587228270638-2c0ac062c01a?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
+        return '/img/thunder.webp'
     }
 
     if (weatherId >= 300 && weatherId <= 350) {
         // rainy-4
-        return 'https://images.unsplash.com/photo-1496034663057-6245f11be793';
+        return '/img/rainy-4.webp'
     }
 
     if (weatherId >= 500 && weatherId <= 550) {
         // rainy-7
-        return 'https://images.unsplash.com/photo-1433863448220-78aaa064ff47';
+        return '/img/rainy-7.webp'
     }
 
     if (weatherId == 600) {
         // snowwy-4
-        return 'https://images.pexels.com/photos/907284/pexels-photo-907284.jpeg';
+        return '/img/snowwy-4.webp'
     }
 
     if (weatherId == 601) {
         // snowwy-5
-        return 'https://images.pexels.com/photos/904382/pexels-photo-904382.jpeg';
+        return '/img/snowwy-4.webp'
     }
 
     if (weatherId > 601 && weatherId <= 650) {
         // snowwy-6
-        return 'https://images.pexels.com/photos/907284/pexels-photo-907284.jpeg';
+        return '/img/snowwy-6.webp'
     }
 
     if (weatherId == 800) {
         // day
-        return 'https://e3.365dm.com/23/11/2048x1152/skynews-uk-weather-rainbow_6375407.jpg';
+        return '/img/bg-default.webp'
     }
 
     if (weatherId == 801) {
         // cloudy-day-1
-        return 'https://cdn.pixabay.com/photo/2023/03/08/17/01/darling-7838296_1280.jpg';
+        return '/img/cloudy-day-1.webp'
     }
 
     if (weatherId == 802) {
         // cloudy-day-2
-        return 'https://cdn.pixabay.com/photo/2023/03/08/17/01/darling-7838296_1280.jpg';
+        return '/img/cloudy-day-1.webp'
     }
 
     if (weatherId > 802) {
         // cloudy-day-3
-        return 'https://cdn.pixabay.com/photo/2023/03/08/17/01/darling-7838296_1280.jpg';
+        return '/img/cloudy-day-1.webp'
     }
 
-    return 'https://e3.365dm.com/23/11/2048x1152/skynews-uk-weather-rainbow_6375407.jpg'
+    return '/img/day.webp'
 }
